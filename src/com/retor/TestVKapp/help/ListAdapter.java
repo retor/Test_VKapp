2484fae3c2ac9947b67763ac92c96ed8f3838149
retor <retor@mail.ru>
2014-09-25 22:49:02 +0400

    1

--- src/com/retor/TestVKapp/help/ListAdapter.java
+++ src/com/retor/TestVKapp/help/ListAdapter.java
@@ -79,6 +79,7 @@ public class ListAdapter extends BaseAdapter {
         holder.comments.setText(String.valueOf(getItem(position).getComments_count()));
         holder.likes.setText(String.valueOf(getItem(position).getLikes_count()));
         return out;
+        
     }
 
     private class ViewHolder{
